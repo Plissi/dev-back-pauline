d2d8a25d106f4d4120dbd21dd809932319a8b8c5
Lock Minkoh Pauline Flore <lockflore@gmail.com>
2022-12-03 08:54:13 +0100

    added authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('login', 255)->unique();
+            $table->string('password', 255);
+            $table->string('name', 255)->nullable()->default(null);
+            $table->string('email', 255)->unique();
+            $table->timestamp('email_verified_at')->nullable()->default(null);
+            $table->timestamp('deleted_at')->nullable()->default(null);
             $table->timestamps();
         });
     }
